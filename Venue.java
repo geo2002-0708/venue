59db7188bfa4dec39f3281c37140599b46a5b3ae
geo2002-0708 <geofreymasumbuko2002@gmail.com>
2026-02-09 17:01:02 +0300

    Create Venue.java for Venue entity

---
+++ Venue.java
@@ -0,0 +1,23 @@
+public class Venue {
+    private String name;
+    private String location;
+    private int capacity;
+
+    public Venue(String name, String location, int capacity) {
+        this.name = name;
+        this.location = location;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
